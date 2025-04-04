8cc2355d40c9ce983c01ed5f82d5a04b64160cfa
suleymansurucu <suleymansurucu95@gmail.com>
2025-04-04 15:54:44 -0400

    feat(caregiver): ✨created notification permission screen

---
+++ lib/views/auth/request_notification_permission.dart
@@ -0,0 +1,85 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class RequestNotificationPermission extends StatefulWidget {
+  const RequestNotificationPermission({super.key});
+
+  @override
+  State<RequestNotificationPermission> createState() =>
+      _RequestNotificationPermissionState();
+}
+
+class _RequestNotificationPermissionState
+    extends State<RequestNotificationPermission> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Color(0xFFFFF9C4), Color(0xFFFFE0B2), Color(0xFFFFCDD2)],
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 16.h),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Center(
+                child: Column(
+
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/reminder_logo.png',
+                      fit: BoxFit.fitWidth,
+                      width: 200.w,
+                      height: 200.h,
+                    ),
+                    SizedBox(height: 10.h),
+                    Text(
+                      context.tr("do_not_miss_a_moment"),
+                      textAlign:TextAlign.center,
+                      style: Theme.of(context).textTheme.headlineLarge,
+                    ),
+                    SizedBox(height: 20.h),
+                    Text(
+                      context.tr("get_gentle_reminders"),
+                      textAlign:TextAlign.center,style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    SizedBox(height: 40.h),
+
+                    ElevatedButton(
+                      onPressed: () {
+                        // TODO: Sign up logic
+                      },
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(double.infinity, 40.h),
+                        backgroundColor: Colors.pinkAccent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.r),
+                        ),
+                      ),
+                      child: Text(
+                        context.tr("allow_notifications"),
+                        style: TextStyle(
+                          fontSize: 16.sp,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
