6d9c4be5e4db2f988b275ec8029c5b90df3c34bd
suleymansurucu <suleymansurucu95@gmail.com>
2025-04-21 19:55:52 -0400

    feat: ✨ created messages constant

---
+++ lib/core/constant/message_constants.dart
@@ -0,0 +1,9 @@
+class MessageConstants {
+  static const String requiredFields = "Please fill in all required fields.";
+  static const String saveSuccess = "Saved successfully.";
+  static const String uploadError = "Failed to upload image.";
+  static const String uploadSuccess = "Your image has been uploaded successfully.";
+  static const String updateSuccess= "Your baby information has been updated successfully";
+  static const String noChanges = "No changes detected.";
+  static const String deleteConfirm = "Are you sure you want to delete?";
+}
