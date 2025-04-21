a09827632425131d526ef69f58fb4c696c45953f
suleymansurucu <suleymansurucu95@gmail.com>
2025-04-21 19:46:54 -0400

    feat: ✨ added baby bloc. new function for baby like update, get, etc.

--- lib/data/repositories/baby_repository.dart
+++ lib/data/repositories/baby_repository.dart
@@ -1,5 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter_sara_baby_tracker_and_sound/data/models/baby_model.dart';
 
 abstract class BabyRepository{
   Future<void> createBaby(BabyModel baby);
+  Future<BabyModel?> getSelectedBaby(String? babyID);
+  Future<List<BabyModel>> getBabies();
+  Future<String?> uploadBabyImage(String babyID);
+  Future<void> updateBaby(String babyID, Map<String, dynamic> updatedFields);
+  Future<void> deleteBaby(String babyID);
+  Future<String?> uploadBabyImageToFile(String babyID, File file);
+
+
+
 }
--- lib/data/repositories/baby_repsitory_impl.dart
+++ lib/data/repositories/baby_repsitory_impl.dart
@@ -1,12 +1,113 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_sara_baby_tracker_and_sound/data/models/baby_model.dart';
 import 'package:flutter_sara_baby_tracker_and_sound/data/repositories/baby_repository.dart';
+import 'package:image_picker/image_picker.dart';
 
-class BabyRepositoryImpl extends BabyRepository{
-  final FirebaseFirestore _firestore=FirebaseFirestore.instance;
+class BabyRepositoryImpl extends BabyRepository {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   @override
-  Future<void> createBaby(BabyModel baby) async{
-   await _firestore.collection("babies").doc(baby.babyID).set(baby.toMap());
+  Future<void> createBaby(BabyModel baby) async {
+    await _firestore.collection("babies").doc(baby.babyID).set(baby.toMap());
   }
 
+  @override
+  Future<BabyModel?> getSelectedBaby(String? babyID) async {
+    final String? userID = _auth.currentUser?.uid;
+    if (userID == null) return null;
+
+    try {
+      if (babyID == null) {
+        final querySnapshot =
+            await _firestore
+                .collection("babies")
+                .where('userID', isEqualTo: userID)
+                .limit(1)
+                .get();
+
+        if (querySnapshot.docs.isEmpty) return null;
+
+        return BabyModel.fromMap(querySnapshot.docs.first.data());
+      } else {
+        final docSnapshot =
+            await _firestore.collection("babies").doc(babyID).get();
+
+        final data = docSnapshot.data();
+        if (data == null || data.isEmpty) return null;
+
+        return BabyModel.fromMap(data);
+      }
+    } catch (e) {
+      print("Error fetching baby: $e");
+      return null;
+    }
+  }
+
+  @override
+  Future<List<BabyModel>> getBabies() async {
+    final String? userID = _auth.currentUser?.uid;
+    if (userID == null) return [];
+    var userMap=(await FirebaseFirestore.instance.collection('users').doc(userID).get()).data();
+    final String parentID=userMap!['parentID'];
+    try {
+      var snapshot =
+          await _firestore
+              .collection("babies")
+              .where('parentID', isEqualTo: parentID)
+              .get();
+      return snapshot.docs.map((doc) {
+        final data = doc.data();
+        return BabyModel.fromMap(data);
+      }).toList();
+    } catch (e) {
+      print("Error fetching baby: $e");
+      return [];
+    }
+  }
+
+
+  /// Upload image to FIRESTORE and Storage !!!
+  @override
+  Future<String?> uploadBabyImage(String babyID) async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile == null) return null;
+    final file = File(pickedFile.path);
+    final ref = FirebaseStorage.instance.ref().child('baby_image/$babyID.jpg');
+    await ref.putFile(file);
+    final downloadUrl = await ref.getDownloadURL();
+    await FirebaseFirestore.instance.collection('babies').doc(babyID).update({
+      'imageUrl': downloadUrl,
+    });
+    return downloadUrl;
+  }
+
+  Future<void> updateBaby(
+    String babyID,
+    Map<String, dynamic> updatedFields,
+  ) async {
+    await _firestore
+        .collection('babies')
+        .doc(babyID)
+        .set(updatedFields, SetOptions(merge: true));
+  }
+
+  @override
+  Future<void> deleteBaby(String babyID) async {
+   await _firestore.collection('babies').doc(babyID).delete();
+  }
+
+  /// Upload Image to JUST Storage and get URL LINK!!!
+  Future<String?> uploadBabyImageToFile(String babyID, File file) async {
+
+    final ref = FirebaseStorage.instance.ref().child('baby_image/$babyID.jpg');
+    await ref.putFile(file);
+    final downloadUrl = await ref.getDownloadURL();
+    return downloadUrl;
+  }
 }
