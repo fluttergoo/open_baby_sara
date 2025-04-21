45af6c563337fac15300a0629e313d0529abe87f
suleymansurucu <suleymansurucu95@gmail.com>
2025-04-21 19:52:56 -0400

    feat: ✨ created user can change password on page

---
+++ lib/views/account/change_password_page.dart
@@ -0,0 +1,133 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_sara_baby_tracker_and_sound/app/routes/navigation_wrapper.dart';
+import 'package:flutter_sara_baby_tracker_and_sound/blocs/auth/auth_bloc.dart';
+import 'package:flutter_sara_baby_tracker_and_sound/widgets/build_custom_snack_bar.dart';
+import 'package:flutter_sara_baby_tracker_and_sound/widgets/custom_text_form_field.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ChangePasswordPage extends StatefulWidget {
+  const ChangePasswordPage({super.key});
+
+  @override
+  State<ChangePasswordPage> createState() => _ChangePasswordPageState();
+}
+
+class _ChangePasswordPageState extends State<ChangePasswordPage> {
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Change Password',
+          style: Theme.of(
+            context,
+          ).textTheme.titleMedium!.copyWith(color: Colors.deepPurpleAccent),
+        ),
+        iconTheme: IconThemeData(color: Colors.purple),
+        elevation: 2,
+      ),
+      body: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is PasswordChanged) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(buildCustomSnackBar(state.message));
+          }
+          Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_)=>NavigationWrapper()));
+        },
+        child: BlocBuilder<AuthBloc, AuthState>(
+          builder: (context, state) {
+            return Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(0xFFF5E6E8), Color(0xFFF6F5F5)],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+              child: SafeArea(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 24.w,
+                    vertical: 16.h,
+                  ),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        CustomTextFormField(
+                          hintText: context.tr("password"),
+                          controller: _passwordController,
+                          isPassword: true,
+                          validator:
+                              (value) =>
+                                  value != null && value.length > 5
+                                      ? null
+                                      : context.tr("min_6_characters"),
+                        ),
+                        SizedBox(height: 7.h),
+                        CustomTextFormField(
+                          hintText: context.tr("confirm_password"),
+                          controller: _confirmPasswordController,
+                          isPassword: true,
+                          validator:
+                              (value) =>
+                                  value == _passwordController.text
+                                      ? null
+                                      : context.tr("passwords_do_not_match"),
+                        ),
+                        SizedBox(height: 20.h),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 45.h,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              _onPressedSave();
+                            },
+                            child: Text(
+                              'Save',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18.sp,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  void _onPressedSave() {
+    if (_formKey.currentState!.validate()) {
+      if (_passwordController.text != _confirmPasswordController.text) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(context.tr('passwords_do_not_match'))),
+        );
+        return;
+      } else {
+        context.read<AuthBloc>().add(
+          ChangePassword(password: _passwordController.text),
+        );
+      }
+    } else {
+      return;
+    }
+  }
+}
