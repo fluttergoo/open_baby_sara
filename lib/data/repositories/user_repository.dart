456bae94f12f7d311d39544d4d1a61b7ab5bbf10
suleymansurucu <suleymansurucu95@gmail.com>
2025-04-21 19:49:40 -0400

    feat: ✨ improving user auth.

--- lib/data/repositories/user_repository.dart
+++ lib/data/repositories/user_repository.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_sara_baby_tracker_and_sound/data/models/invite_model.dart';
 import 'package:flutter_sara_baby_tracker_and_sound/data/models/user_model.dart';
 
 abstract class UserRepository{
@@ -6,4 +6,8 @@ abstract class UserRepository{
   Future<void> createUserInFireStore(UserModel user);
   Future<void> signInEmailAndPassword(String email, String password);
   Future<UserModel?> getCurrentUser();
+  Future<void> signOut();
+  Future<void> addCaregiverInUser(InviteModel caregiver);
+  Future<void> changePassword(String password);
+  Future<void>deleteUser();
 }
--- lib/data/repositories/user_repository_impl.dart
+++ lib/data/repositories/user_repository_impl.dart
@@ -1,11 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_sara_baby_tracker_and_sound/data/models/invite_model.dart';
 import 'package:flutter_sara_baby_tracker_and_sound/data/models/user_model.dart';
 import 'package:flutter_sara_baby_tracker_and_sound/data/repositories/user_repository.dart';
 
 class UserRepositoryImpl implements UserRepository {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final FirebaseAuth _firebaseAuth=FirebaseAuth.instance;
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   @override
   Future<void> createUserInFireStore(UserModel user) async {
@@ -13,13 +14,16 @@ class UserRepositoryImpl implements UserRepository {
   }
 
   @override
-  Future<void> signInEmailAndPassword(String email, String password) async{
-  await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+  Future<void> signInEmailAndPassword(String email, String password) async {
+    await _firebaseAuth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
   }
 
   @override
-  Future<UserModel?> getCurrentUser() async{
-    final user=_firebaseAuth.currentUser;
+  Future<UserModel?> getCurrentUser() async {
+    final user = _firebaseAuth.currentUser;
     if (user != null) {
       final doc = await _firestore.collection('users').doc(user.uid).get();
       return UserModel.fromMap(doc.data()!);
@@ -27,5 +31,43 @@ class UserRepositoryImpl implements UserRepository {
     return null;
   }
 
+  @override
+  Future<void> signOut() async {
+    final user = _firebaseAuth.currentUser;
+    if (user != null) {
+      await _firebaseAuth.signOut();
+    }
+  }
 
+  @override
+  Future<void> addCaregiverInUser(InviteModel caregiver) async {
+    final user = _firebaseAuth.currentUser;
+    await _firestore.collection('users').doc(user!.uid).update({
+      'caregivers': FieldValue.arrayUnion([caregiver.toMap()]),
+    });
+  }
+
+  @override
+  Future<void> changePassword(String password) async {
+    final user = _firebaseAuth.currentUser;
+    try{
+      await user!.updatePassword(password);
+    }on FirebaseAuthException catch (e){
+      throw e.toString();
+    }catch (e){
+     e.toString();
+    }
+  }
+
+  @override
+  Future<void> deleteUser() async{
+    final user = _firebaseAuth.currentUser;
+    try{
+      await user!.delete();
+    }on FirebaseAuthException catch (e){
+    throw e.toString();
+    }catch (e){
+    e.toString();
+    }
+  }
 }
