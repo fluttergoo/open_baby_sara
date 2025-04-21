78c393536a5bc6bd910902becfab30a295c0244a
suleymansurucu <suleymansurucu95@gmail.com>
2025-04-21 19:54:44 -0400

    feat: ✨ created home page

---
+++ lib/views/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_sara_baby_tracker_and_sound/app/routes/navigation_wrapper.dart';
+import 'package:flutter_sara_baby_tracker_and_sound/blocs/theme/theme_bloc.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('home')));
+  }
+}
