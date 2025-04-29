a50af8ec7df4d82861139f29d99dcb2530865164
suleymansurucu <suleymansurucu95@gmail.com>
2025-04-28 21:39:16 -0400

    feat: ✨ added Timer Repo, Activity Repo, Activity Service

--- lib/core/locator.dart
+++ lib/core/locator.dart
@@ -1,17 +1,36 @@
+import 'package:flutter_sara_baby_tracker_and_sound/data/repositories/activity_repository_impl.dart';
+import 'package:flutter_sara_baby_tracker_and_sound/data/repositories/activity_reposityory.dart';
 import 'package:flutter_sara_baby_tracker_and_sound/data/repositories/baby_repository.dart';
 import 'package:flutter_sara_baby_tracker_and_sound/data/repositories/baby_repsitory_impl.dart';
 import 'package:flutter_sara_baby_tracker_and_sound/data/repositories/caregiver_repository.dart';
 import 'package:flutter_sara_baby_tracker_and_sound/data/repositories/caregiver_repository_impl.dart';
+import 'package:flutter_sara_baby_tracker_and_sound/data/repositories/timer_repository.dart';
+import 'package:flutter_sara_baby_tracker_and_sound/data/repositories/timer_repository_impl.dart';
 import 'package:flutter_sara_baby_tracker_and_sound/data/repositories/user_repository.dart';
 import 'package:flutter_sara_baby_tracker_and_sound/data/repositories/user_repository_impl.dart';
+import 'package:flutter_sara_baby_tracker_and_sound/data/services/firebase/activity_service.dart';
+import 'package:flutter_sara_baby_tracker_and_sound/data/services/firebase/activity_service_impl.dart';
 import 'package:flutter_sara_baby_tracker_and_sound/data/services/firebase/auth_service.dart';
 import 'package:get_it/get_it.dart';
 
-final getIt =GetIt.instance;
-void setupLocator(){
-  getIt.registerLazySingleton<AuthService>(()=>AuthService());
-  getIt.registerLazySingleton<UserRepository>(()=>UserRepositoryImpl());
-  getIt.registerLazySingleton<BabyRepository>(()=>BabyRepositoryImpl());
-  getIt.registerLazySingleton<CaregiverRepository>(()=>CaregiverRepositoryImpl());
+import '../data/services/local_database/local_database_service.dart';
 
+final getIt = GetIt.instance;
+
+Future<void> setupLocator() async {
+  final database = await LocalDatabaseService.database;
+
+  getIt.registerLazySingleton<AuthService>(() => AuthService());
+  getIt.registerLazySingleton<UserRepository>(() => UserRepositoryImpl());
+  getIt.registerLazySingleton<BabyRepository>(() => BabyRepositoryImpl());
+  getIt.registerLazySingleton<CaregiverRepository>(
+    () => CaregiverRepositoryImpl(),
+  );
+  getIt.registerLazySingleton<TimerRepository>(
+    () => TimerRepositoryImpl(database: database),
+  );
+  getIt.registerLazySingleton<ActivityRepository>(
+    () => ActivityRepositoryImpl(database: database),
+  );
+  getIt.registerLazySingleton<ActivityService>(() => ActivityServiceImpl());
 }
