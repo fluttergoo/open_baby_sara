5d113da8966d6b1a96b29bafdceac358be861958
suleymansurucu <suleymansurucu95@gmail.com>
2025-05-06 21:59:01 -0400

    feat: ✨created Wet Dirty Dry Selector using Container

---
+++ lib/widgets/wet_dirty_dry_selector.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class WetDirtyDrySelector extends StatefulWidget {
+  final void Function(List<String> selectedValues) onChanged;
+
+  const WetDirtyDrySelector({super.key, required this.onChanged});
+
+  @override
+  State<WetDirtyDrySelector> createState() => _WetDirtyDrySelectorState();
+}
+
+class _WetDirtyDrySelectorState extends State<WetDirtyDrySelector> {
+  List<String> selected = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        buildToggleButton("Wet"),
+        buildToggleButton("Dirty"),
+        buildToggleButton("Dry"),
+      ],
+    );
+  }
+
+  void toggleSelection(String value) {
+    setState(() {
+      if (selected.contains(value)) {
+        selected.remove(value);
+      } else if (value == 'Dry') {
+        selected.clear();
+        selected.add(value);
+      } else {
+        selected.remove('Dry');
+        selected.add(value);
+      }
+      widget.onChanged(selected); // Seçilen değerleri dışarıya aktar
+    });
+  }
+
+  Widget buildToggleButton(String label) {
+    final isSelected = selected.contains(label);
+
+    return GestureDetector(
+      onTap: () => toggleSelection(label),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+        margin: const EdgeInsets.symmetric(horizontal: 5),
+        decoration: BoxDecoration(
+          color: isSelected ? Theme.of(context).primaryColor : Colors.white,
+          borderRadius: BorderRadius.circular(10.r),
+          border: Border.all(color: Theme.of(context).primaryColor),
+        ),
+        child: Center(
+          child: Text(
+            label,
+            style: Theme.of(context).textTheme.titleMedium?.copyWith(
+              color: isSelected ? Colors.white : Theme.of(context).primaryColor,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
