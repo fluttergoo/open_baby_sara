29c358930677f272ed83838b54e2eb3186fe731e
suleymansurucu <suleymansurucu95@gmail.com>
2025-05-09 18:59:10 -0400

    feat: ✨added new variable to Activity Type enum. That is growth

--- lib/data/models/activity_model.dart
+++ lib/data/models/activity_model.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-enum ActivityType { breastFeed, bottleFeed, solids, pumpTotal,pumpLeftRight, diaper, sleep }
+enum ActivityType { breastFeed, bottleFeed, solids, pumpTotal,pumpLeftRight, diaper, sleep, growth}
 
 class ActivityModel {
   final String activityID;
