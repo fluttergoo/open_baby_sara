4cfa18b10eab1dba0ab0b01432f48700ae4afdd8
suleymansurucu <suleymansurucu95@gmail.com>
2025-05-19 20:42:19 -0400

    feat: ✨created activity type lists constants

---
+++ lib/core/constant/activity_constants.dart
@@ -0,0 +1,32 @@
+
+const Map<String, List<String>> activityTypeMap = {
+  'All Activities': [],
+  'Feed': ['breastFeed', 'bottleFeed', 'solids'],
+  'Pump': ['pumpTotal', 'pumpLeftRight'],
+  'Sleep': ['sleep'],
+  'Diaper': ['diaper'],
+  'Growth': ['growth'],
+  'Baby Firsts': ['babyFirsts'],
+  'Teething': ['teething'],
+  'Medication': ['medication'],
+  'Fever': ['fever'],
+  'Vaccination': ['vaccination'],
+  'Doctor Visit': ['doctorVisit'],
+};
+
+const Map<String, String> activityIconMap = {
+  'breastFeed': 'feed',
+  'bottleFeed': 'feed',
+  'solids': 'feed',
+  'pumpTotal': 'pump',
+  'pumpLeftRight': 'pump',
+  'sleep': 'sleep',
+  'diaper': 'diaper',
+  'growth': 'growth',
+  'babyFirsts': 'babyFirsts',
+  'teething': 'teething',
+  'medication': 'medication',
+  'fever': 'fever',
+  'vaccination': 'vaccination',
+  'doctorVisit': 'doctorVisit',
+};
