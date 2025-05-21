9fb37a0248d192459ea180af0bd56de6e35e8230
suleymansurucu <suleymansurucu95@gmail.com>
2025-05-20 21:02:33 -0400

    feat: ✨ created relaxing sounds constants

---
+++ lib/core/constant/sounds_constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_sara_baby_tracker_and_sound/data/models/relaxing_sound_model.dart';
+
+final List<RelaxingSoundModel> sounds = [
+  RelaxingSoundModel(title: "White Noise", assetPath: "assets/sounds/white_noise.mp3", iconAssetPath: "assets/icons/white_noise.png"),
+  RelaxingSoundModel(title: "Rain", assetPath: "assets/sounds/rain.mp3", iconAssetPath: "assets/icons/rain.png"),
+  RelaxingSoundModel(title: "Heartbeat", assetPath: "assets/sounds/heartbeat.mp3", iconAssetPath: "assets/icons/heartbeat.png"),
+  RelaxingSoundModel(title: "Shushing", assetPath: "assets/sounds/shushing.mp3", iconAssetPath: "assets/icons/shushing.png"),
+  RelaxingSoundModel(title: "Ocean Waves", assetPath: "assets/sounds/ocean_waves.mp3", iconAssetPath: "assets/icons/ocean_wave.png"),
+  RelaxingSoundModel(title: "Forest Birds", assetPath: "assets/sounds/bird_sounds.mp3", iconAssetPath: "assets/icons/bird_sounds.png"),
+  RelaxingSoundModel(title: "Lullaby", assetPath: "assets/sounds/la_la_la_my_sunshine.mp3", iconAssetPath: "assets/icons/la_la_la_my_sunshine.png"),
+  RelaxingSoundModel(title: "Fan", assetPath: "assets/sounds/fan_running.mp3", iconAssetPath: "assets/icons/fan_running.png"),
+];
