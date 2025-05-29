cfeeae76f78dda0264857dc223a65166b83c2c9e
suleymansurucu <suleymansurucu95@gmail.com>
2025-05-29 14:27:24 -0400

    feat: ✨added new variable for Flushbar class.

--- lib/widgets/custom_show_flush_bar.dart
+++ lib/widgets/custom_show_flush_bar.dart
@@ -1,21 +1,24 @@
 import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 
-void showCustomFlushbar(BuildContext context,String title, String message, IconData icon) {
+Future<void> showCustomFlushbar(
+  BuildContext context,
+  String title,
+  String message,
+  IconData icon, {
+  Color? color = const Color(0xFFEF5350),
+}) async {
   Flushbar(
     margin: const EdgeInsets.all(16),
     padding: const EdgeInsets.all(16),
     borderRadius: BorderRadius.circular(16),
-    backgroundColor: Colors.red.shade400,
+    backgroundColor: color!,
     titleText: Text(
       title,
       style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
     ),
-    messageText: Text(
-      message,
-      style: TextStyle(color: Colors.white),
-    ),
-    icon: Icon(icon,color: Colors.white,),
+    messageText: Text(message, style: TextStyle(color: Colors.white)),
+    icon: Icon(icon, color: Colors.white),
     duration: Duration(seconds: 3),
   ).show(context);
 }
