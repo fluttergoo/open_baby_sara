ce7bd56bac1fc02e0f7d1a843c12ef134dcdd8f0
suleymansurucu <suleymansurucu95@gmail.com>
2025-05-29 14:35:58 -0400

    feat: ✨created recipe bloc

---
+++ lib/blocs/recipe/recipe_state.dart
@@ -0,0 +1,19 @@
+part of 'recipe_bloc.dart';
+
+@immutable
+sealed class RecipeState {}
+
+final class RecipeInitial extends RecipeState {}
+
+class RecipeLoaded extends RecipeState{
+  final List<RecipeModel> recipes;
+
+  RecipeLoaded({required this.recipes});
+}
+class RecipeLoading extends RecipeState{}
+class RecipeError extends RecipeState{
+  final String message;
+
+  RecipeError({required this.message});
+
+}
