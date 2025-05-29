1761356e3642ab007a1ad4a3d2ba2bce3f6227f0
suleymansurucu <suleymansurucu95@gmail.com>
2025-05-29 14:37:07 -0400

    feat: ✨changed the directory name.

--- lib/views/auth/request_notification_permission.dart
+++ lib/views/auth/request_notification_permission.dart
@@ -1,7 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sara_baby_tracker_and_sound/app/routes/navigation_wrapper.dart';
-import 'package:flutter_sara_baby_tracker_and_sound/views/home.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class RequestNotificationPermission extends StatefulWidget {
