f33f06a976994f6f66ae59517637325ce19809d3
suleymansurucu <suleymansurucu95@gmail.com>
2025-05-29 14:25:24 -0400

    feat: ✨ improved flushbar package

--- lib/widgets/custom_avatar.dart
+++ lib/widgets/custom_avatar.dart
@@ -4,96 +4,49 @@ import 'package:flutter/material.dart';
 import 'package:flutter_sara_baby_tracker_and_sound/core/app_colors.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class CustomAvatar extends StatefulWidget {
-  String? imageUrl;
-  VoidCallback? onTap;
+class CustomAvatar extends StatelessWidget {
+  final String? imagePath;
+  final VoidCallback? onTap;
   final double size;
-  final File? localFile;
 
-
-  CustomAvatar({super.key, this.imageUrl, this.onTap, this.size = 100, this.localFile});
-
-  @override
-  State<CustomAvatar> createState() => _CustomAvatarState();
-}
-
-class _CustomAvatarState extends State<CustomAvatar> {
-  bool _isLoading = true;
-  late ImageProvider imageProvider;
-
+  const CustomAvatar({
+    super.key,
+    this.imagePath,
+    this.onTap,
+    this.size = 100,
+  });
 
   @override
   Widget build(BuildContext context) {
+    ImageProvider imageProvider;
 
-    if (widget.localFile != null) {
-      imageProvider = FileImage(widget.localFile!);
-    } else if (widget.imageUrl != null && widget.imageUrl!.isNotEmpty) {
-      imageProvider = NetworkImage(widget.imageUrl!);
+    if (imagePath != null && File(imagePath!).existsSync()) {
+      imageProvider = FileImage(File(imagePath!));
     } else {
       imageProvider = const AssetImage('assets/images/default_baby.png');
     }
     return GestureDetector(
-      onTap: widget.onTap,
+      onTap: onTap,
       child: Container(
-        width: widget.size,
-        height: widget.size,
+        width: size,
+        height: size,
         margin: const EdgeInsets.only(top: 16, right: 16),
         decoration: BoxDecoration(
           color: AppColors.vaccineColor,
           shape: BoxShape.circle,
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 8,
-              offset: Offset(2, 4),
-            ),
-          ],
-          border: Border.all(
-            color: Colors.white,
-            width: 3.w,
-          ),
+          boxShadow: const [BoxShadow(color: Colors.black26, blurRadius: 8, offset: Offset(2, 4))],
+          border: Border.all(color: Colors.white, width: 3.w),
         ),
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            /// Resim
-            ClipOval(
-              child: Image(
-                image: imageProvider,
-                fit: BoxFit.cover,
-                width: widget.size,
-                height: widget.size,
-                loadingBuilder: (context, child, loadingProgress) {
-                  if (loadingProgress == null) {
-                    if (_isLoading) {
-                      WidgetsBinding.instance.addPostFrameCallback((_) {
-                        if (mounted) {
-                          setState(() {
-                            _isLoading = false;
-                          });
-                        }
-                      });
-                    }
-                    return child;
-                  } else {
-                    return Container();
-                  }
-                },
-              ),
-            ),
-
-            if (_isLoading)
-              const Positioned.fill(
-                child: Center(
-                  child: CircularProgressIndicator(
-                    strokeWidth: 2,
-                    color: Colors.purple,
-                  ),
-                ),
-              ),
-          ],
+        child: ClipOval(
+          child: Image(
+            image: imageProvider,
+            fit: BoxFit.cover,
+            width: size,
+            height: size,
+          ),
         ),
       ),
     );
   }
 }
+
