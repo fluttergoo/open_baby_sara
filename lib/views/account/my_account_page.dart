cfdf1e9e5cece078994ef592fe6a56a576b94233
suleymansurucu <suleymansurucu95@gmail.com>
2025-05-29 14:32:02 -0400

    feat: ✨improved UI text to support multiple languages using Easy Localization

--- lib/views/account/my_account_page.dart
+++ lib/views/account/my_account_page.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_sara_baby_tracker_and_sound/app/routes/app_router.dart';
@@ -22,105 +23,159 @@ class MyAccountPage extends StatelessWidget {
         elevation: 2,
       ),
       body: BlocBuilder<AuthBloc, AuthState>(
-  builder: (context, state) {
-    return Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color(0xFFF5E6E8), Color(0xFFF6F5F5)],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        ),
-        child: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 16.h),
-            child: Column(children: [
-              ListTile(
-                title: Text(
-                  'Change Password',style: Theme.of(context).textTheme.titleMedium?.copyWith(fontSize: 16.sp),
-                ),
-                trailing: Icon(Icons.arrow_forward_ios_rounded, size: 16.sp,),
-                onTap: () {
-                  Navigator.of(context).pushNamed(AppRoutes.changePassword);
-                },
+        builder: (context, state) {
+          return Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xFFF5E6E8), Color(0xFFF6F5F5)],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
               ),
-              Divider(
-                color: Colors.grey.shade400,
-                thickness: 0.8,
-                height: 10,
-              ),
-              ListTile(
-                title: Text(
-                  'FAQs',style: Theme.of(context).textTheme.titleMedium?.copyWith(fontSize: 16.sp),
-                ),
-                trailing: Icon(Icons.arrow_forward_ios_rounded, size: 16.sp,),
+            ),
+            child: SafeArea(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 16.h),
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Text(
+                        context.tr('change_password'),
+                        style: Theme.of(
+                          context,
+                        ).textTheme.titleMedium?.copyWith(fontSize: 16.sp),
+                      ),
+                      trailing: Icon(
+                        Icons.arrow_forward_ios_rounded,
+                        size: 16.sp,
+                      ),
+                      onTap: () {
+                        Navigator.of(
+                          context,
+                        ).pushNamed(AppRoutes.changePassword);
+                      },
+                    ),
+                    Divider(
+                      color: Colors.grey.shade400,
+                      thickness: 0.8,
+                      height: 10,
+                    ),
+                    ListTile(
+                      title: Text(
+                        context.tr('faq_title'),
+                        style: Theme.of(
+                          context,
+                        ).textTheme.titleMedium?.copyWith(fontSize: 16.sp),
+                      ),
+                      trailing: Icon(
+                        Icons.arrow_forward_ios_rounded,
+                        size: 16.sp,
+                      ),
 
-                onTap: () {},
-              ),
-              Divider(
-                color: Colors.grey.shade400,
-                thickness: 0.8,
-                height: 10,
-              ),
-              ListTile(
-                title: Text(
-                  'Contact Us',style: Theme.of(context).textTheme.titleMedium?.copyWith(fontSize: 16.sp),
+                      onTap: () {
+                        Navigator.of(context).pushNamed(AppRoutes.faq);
+                      },
+                    ),
+                    Divider(
+                      color: Colors.grey.shade400,
+                      thickness: 0.8,
+                      height: 10,
+                    ),
+                    ListTile(
+                      title: Text(
+                        context.tr('contact_us'),
+                        style: Theme.of(
+                          context,
+                        ).textTheme.titleMedium?.copyWith(fontSize: 16.sp),
+                      ),
+                      trailing: Text(
+                        'support@sarababy.app',
+                        style: Theme.of(context).textTheme.titleSmall,
+                      ),
+                      onTap: () {},
+                    ),
+                    Divider(
+                      color: Colors.grey.shade400,
+                      thickness: 0.8,
+                      height: 10,
+                    ),
+                    ListTile(
+                      title: Text(
+                        context.tr('version'),
+                        style: Theme.of(
+                          context,
+                        ).textTheme.titleMedium?.copyWith(fontSize: 16.sp),
+                      ),
+                      trailing: Text(
+                        '1.0.0,',
+                        style: Theme.of(context).textTheme.titleSmall,
+                      ),
+                      onTap: () {},
+                    ),
+                    Divider(
+                      color: Colors.grey.shade400,
+                      thickness: 0.8,
+                      height: 10,
+                    ),
+                    ListTile(
+                      title: Text(
+                        context.tr('legal_title'),
+                        style: Theme.of(
+                          context,
+                        ).textTheme.titleMedium?.copyWith(fontSize: 16.sp),
+                      ),
+                      trailing: Icon(
+                        Icons.arrow_forward_ios_rounded,
+                        size: 16.sp,
+                      ),
+                      onTap: () {
+                        Navigator.of(context).pushNamed(AppRoutes.legalPage);
+                      },
+                    ),
+                    Divider(
+                      color: Colors.grey.shade400,
+                      thickness: 0.8,
+                      height: 10,
+                    ),
+                    ListTile(
+                      title: Text(
+                        context.tr('delete_account'),
+                        style: Theme.of(
+                          context,
+                        ).textTheme.titleMedium?.copyWith(
+                          fontSize: 16.sp,
+                          color: Colors.redAccent,
+                        ),
+                      ),
+                      trailing: Icon(
+                        Icons.arrow_forward_ios_rounded,
+                        size: 16.sp,
+                      ),
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return buildCustomAlertDialog(
+                              context: context,
+                              cancelButtonTap: () {
+                                Navigator.of(context).pop();
+                              },
+                              yesButtonTap: () {
+                                context.read<AuthBloc>().add(DeleteUser());
+                              },
+                            );
+                          },
+                        );
+                      },
+                    ),
+                  ],
                 ),
-                trailing: Text('help@sarababy.com',style: Theme.of(context).textTheme.titleSmall,),
-                onTap: () {},
               ),
-              Divider(
-                color: Colors.grey.shade400,
-                thickness: 0.8,
-                height: 10,
-              ),
-              ListTile(
-                title: Text(
-                  'Version',style: Theme.of(context).textTheme.titleMedium?.copyWith(fontSize: 16.sp),
-                ),
-                trailing: Text('0.0.1,',style: Theme.of(context).textTheme.titleSmall,),
-                onTap: () {},
-              ),
-              Divider(
-                color: Colors.grey.shade400,
-                thickness: 0.8,
-                height: 10,
-              ),
-              ListTile(
-                title: Text(
-                  'Legal',style: Theme.of(context).textTheme.titleMedium?.copyWith(fontSize: 16.sp),
-                ),
-                trailing: Icon(Icons.arrow_forward_ios_rounded, size: 16.sp,),
-                onTap: () {},
-              ),
-              Divider(
-                color: Colors.grey.shade400,
-                thickness: 0.8,
-                height: 10,
-              ),
-              ListTile(
-                title: Text(
-                  'Delete Account',style: Theme.of(context).textTheme.titleMedium?.copyWith(fontSize: 16.sp, color: Colors.redAccent),
-                ),
-                trailing: Icon(Icons.arrow_forward_ios_rounded, size: 16.sp,),
-                onTap: () {
-                  showDialog(context: context, builder: (context){
-                    return buildCustomAlertDialog(context: context, cancelButtonTap: (){
-                      Navigator.of(context).pop();
-                    }, yesButtonTap: (){
-                      context.read<AuthBloc>().add(DeleteUser());
-                    });
-                  });
-                },
-              ),
-            ]),
-          ),
-        ),
-      );
-  },
-),
+            ),
+          );
+        },
+      ),
     );
   }
 }
