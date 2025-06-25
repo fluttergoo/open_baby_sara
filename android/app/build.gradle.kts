5edbf8e19fdb53f36920e59aa5c73717ca3683c3
suleymansurucu <suleymansurucu95@gmail.com>
2025-06-24 21:03:36 -0400

    feat: ✨Update version code and version name in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,8 +31,8 @@
             // For more information, see: https://flutter.dev/to/review-gradle-config.
             minSdk = 23
             targetSdk = flutter.targetSdkVersion
-            versionCode = 3
-            versionName = "1.0.3"
+            versionCode = 4
+            versionName = "1.0.4"
 
         }
 
