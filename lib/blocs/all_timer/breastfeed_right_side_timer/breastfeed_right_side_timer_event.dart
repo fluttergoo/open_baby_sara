70b56f72f9184d562c4f6686f348c05f66622312
suleymansurucu <suleymansurucu95@gmail.com>
2025-11-26 23:25:46 -0500

    Feat: ✨Add start time parameter to SetEndTimeTimer event and update duration calculation

--- lib/blocs/all_timer/breastfeed_right_side_timer/breastfeed_right_side_timer_event.dart
+++ lib/blocs/all_timer/breastfeed_right_side_timer/breastfeed_right_side_timer_event.dart
@@ -25,10 +25,14 @@ class SetStartTimeTimer extends BreastfeedRightSideTimerEvent {
 
 class SetEndTimeTimer extends BreastfeedRightSideTimerEvent {
   final DateTime endTime;
-
+  final DateTime? startTime; // Also send start time so bloc state is correct
   final String activityType;
 
-  SetEndTimeTimer({required this.endTime, required this.activityType});
+  SetEndTimeTimer({
+    required this.endTime,
+    this.startTime,
+    required this.activityType,
+  });
 }
 
 class SetDurationTimer extends BreastfeedRightSideTimerEvent {
